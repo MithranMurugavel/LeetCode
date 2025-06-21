6c467d77fb5b084cda62f8ab4c573543e9cbd572
MithranMurugavel <155570587+MithranMurugavel@users.noreply.github.com>
2025-06-21 22:36:34 +0530

    Time: 1 ms (99.39%), Space: 45.4 MB (57.2%) - LeetHub

--- 2398-check-if-matrix-is-x-matrix/2398-check-if-matrix-is-x-matrix.java
+++ 2398-check-if-matrix-is-x-matrix/2398-check-if-matrix-is-x-matrix.java
@@ -4,12 +4,14 @@ class Solution {
         int row=grid.length;
         int col=grid[0].length;
         for(int i=0;i<col;i++){
+            int val=col-1-i;
             for(int j=0;j<row;j++){
-                if(i==j || col-1-i==j){
-                    if(grid[i][j]==0 || grid[col-1-i][j]==0){
+                if(i==j || val==j){
+                    if(grid[i][j]==0 || grid[val][j]==0){
                                 return false;
                     }
                 }
+                else if(grid[i][j]!=0)return false;
             }
         }
         return true;
