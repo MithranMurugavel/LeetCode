233933f27c2f02cad6d0f782b2200f591aa4eac9
MithranMurugavel <155570587+MithranMurugavel@users.noreply.github.com>
2025-09-13 22:13:17 +0530

    Time: 0 ms (100%), Space: 41.7 MB (8.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -4,7 +4,6 @@ class Solution {
         int rob1=0,rob2=0;
 
         for(int i=0;i<nums.length;i++){
-
             int temp = Math.max(nums[i]+rob1,rob2);
             rob1=rob2;
             rob2=temp;
