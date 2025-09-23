074f93a95a81b8b01865d47a457b19da4efc260b
MithranMurugavel <155570587+MithranMurugavel@users.noreply.github.com>
2025-09-23 14:39:06 +0530

    Time: 1 ms (81.13%), Space: 40.9 MB (72.36%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -2,7 +2,7 @@ class Solution {
     public int reverse(int x) {
         
         int n=x;
-        if(x==-1563847412) return 0;
+        //if(x==-1563847412) return 0;
         if(x<0){
             x=~x+1;
         }
@@ -17,7 +17,7 @@ class Solution {
         if(n>0  && sum>=Integer.MAX_VALUE){
             return 0;
         }else if(n>0  && !(sum>Integer.MAX_VALUE))return (int) sum;
-        else if(n<0  && !(sum<=Integer.MIN_VALUE))return (int) ~sum+1;;
+        else if(n<0  && (sum<Integer.MAX_VALUE))return (int) ~sum+1;;
         return 0;
     }
 }
