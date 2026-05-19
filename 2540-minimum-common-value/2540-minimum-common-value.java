b7db0a8fafd94c5b67e0bfc31efc93c24330ea88
MithranMurugavel <155570587+MithranMurugavel@users.noreply.github.com>
2026-05-19 19:10:32 +0530

    Time: 0 ms (100%), Space: 80.5 MB (48.43%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -3,6 +3,9 @@ class Solution {
         
         int i=0,j=0;
 
+        if (nums1[nums1.length - 1] < nums2[0] || nums2[nums2.length - 1] < nums1[0]) {
+            return -1;
+        }
         while(i<nums1.length && j<nums2.length){
 
             if(nums1[i] == nums2[j]){
